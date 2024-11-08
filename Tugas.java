24f0eafc644b3fa6d03cbb0f6af4c22ac08f3604
ghazwanz <ababilghazwan@gmail.com>
2024-11-08 10:56:38 +0700

    Tugas Jobsheet 10

--- Tugas.java
+++ Tugas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Survei {
+public class Tugas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int jmlPert = 6;
